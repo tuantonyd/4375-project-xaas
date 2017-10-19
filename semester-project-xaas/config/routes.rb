5e0f3f69c6b99f6dc883c10f7f19936eb2b8aacb
Gabriel Gutierrez <gabe@Gabriels-Air.attlocal.net>
2017-10-19 13:36:28 -0500

    pushing prototype

--- semester-project-xaas/config/routes.rb
+++ semester-project-xaas/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :orders, :items
+  devise_for :customers
+  get 'welcome/index'
+  get '/cart', to: "items#cart", as: "cart"
+  get '/checkout', to: "orders#create", as: "check_out"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
 end
