8e7192587e71f2f0c80cc083fd076a7cfebef798
Gabriel Gutierrez <gabe@Gabriels-MacBook-Air.local>
2017-11-01 16:41:28 -0500

    Fixed admin issue

--- semester-project-xaas/app/controllers/admin_controller.rb
+++ semester-project-xaas/app/controllers/admin_controller.rb
@@ -1,5 +1,4 @@
 class AdminController < ApplicationController
-  before_action :authenticate_admin!
   layout 'admin'
   def index
     # Post.paginate(:page => params[:page], :per_page => 30)
--- semester-project-xaas/config/routes.rb
+++ semester-project-xaas/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admins
   resources :orders, :items
   devise_for :customers
   get 'welcome/index'
