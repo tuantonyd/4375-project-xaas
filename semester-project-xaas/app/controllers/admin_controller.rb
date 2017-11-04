d557a365735e95266f5a809f295cb16f342e5320
Gabriel Gutierrez <gabe@Gabriels-MacBook-Air.local>
2017-11-04 10:09:49 -0500

    Fixed admin console, still needs verifcation

--- semester-project-xaas/app/controllers/admin_controller.rb
+++ semester-project-xaas/app/controllers/admin_controller.rb
@@ -4,7 +4,7 @@ class AdminController < ApplicationController
     # Post.paginate(:page => params[:page], :per_page => 30)
     @orders = Order.where(order_status: 1).paginate(:page => params[:page])
      @states_with_orders = Order.joins(:state).pluck(:state_id, :state_name).uniq #Get states that have an order
-     @unavailable_inventory = Admin.unavailable_inventory
+     @unavailable_inventory = ReportingHelper.unavailable_inventory
   end
 
 
